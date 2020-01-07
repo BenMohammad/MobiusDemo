b9fef10309f89ae952b827c9003c0000487bc29b
Ben Mohammad <benmohammad593@gmail.com>
2020-01-07 16:21:40 +0000

    NoOpEffectHandler added...fixed

--- app/src/main/java/com/fukuni/mobiusdemo/architecture/effecthandlers/NoOpEffectHandler.kt
+++ app/src/main/java/com/fukuni/mobiusdemo/architecture/effecthandlers/NoOpEffectHandler.kt
@@ -4,7 +4,7 @@ import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.ObservableTransformer
 
-class   <E>: ObservableTransformer<Nothing, E> {
+class NoOpEffectHandler<E>: ObservableTransformer<Nothing, E> {
 
     override fun apply(upstream: Observable<Nothing>): ObservableSource<E> =
         Observable.empty<E>()
